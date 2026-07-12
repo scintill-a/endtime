bd7240cb063678ccd83375082122fa8254312fb1
scintill-a <scintillaea@gmail.com>
2026-07-12 22:29:50 +0800

    feat(widgets): re-export SessionPickerModal and SessionOverlayModal

--- endtime/widgets/__init__.py
+++ endtime/widgets/__init__.py
@@ -1,6 +1,7 @@
 """UI widgets for Endtime TUI."""
 from endtime.widgets.category_item import CategoryItem
 from endtime.widgets.todo_item import TodoItem
-from endtime.widgets.dialogs import SessionSelectDialog, SessionControlDialog, ConfirmDialog
+from endtime.widgets.session_picker import SessionPickerModal
+from endtime.widgets.session_overlay import SessionOverlayModal
 
-__all__ = ["CategoryItem", "TodoItem", "SessionSelectDialog", "SessionControlDialog", "ConfirmDialog"]
+__all__ = ["CategoryItem", "TodoItem", "SessionPickerModal", "SessionOverlayModal"]
